78673e3d24dd5bd966b741dde123670c7172cde3
llmsilva <86919260+llmsilva@users.noreply.github.com>
2022-10-14 12:10:57 -0300

    Construindo componentes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from './Post';
+
+export function App() {
+    return (
+        <div>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+    )
+}
+
+
